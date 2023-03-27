c806e1338f8f01edf119316f0915e1848cf1fa52
Desparek <desparvek@gmail.com>
2023-03-27 12:45:38 +0200

    add primary colors and font to the tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      theme: {
+        fontFamily: {
+          sans: ['Poppins', 'sans-serif'],
+        },
+      },
+      colors: {
+        primary: {
+          100: '#FDFDFD',
+          200: '#F8F8F8',
+          300: '#949494',
+          400: '#6C6C6C',
+          500: '#444444',
+          600: '#121221',
+        },
+      },
+    },
   },
   plugins: [require('prettier-plugin-tailwindcss')],
 };
