e7ddc971d4939ebf62a4714af884d31b62b8d227
Desparek <desparvek@gmail.com>
2023-03-28 10:50:15 +0200

    add custom.d.ts file and new svgs to the project

--- src/assets/icons/index.js
+++
@@ -1,1 +0,0 @@
-export { default as HamburgerMenuIcon } from './hamburger.svg';
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as HomeIcon } from './home.svg';
+export { ReactComponent as RocketIcon } from './rocket.svg';
+export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as SettingsIcon } from './settings.svg';
