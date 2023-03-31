d6e871e5bc7e201065c80240b52f90d520d73710
Desparek <desparvek@gmail.com>
2023-03-31 11:48:34 +0200

    add mobile options data

---
+++ src/data/mobileOptions.tsx
@@ -0,0 +1,25 @@
+import { HomeIcon, RocketIcon, SettingsIcon } from '@/assets/icons';
+
+const iconStyle: string = 'w-6 h-6';
+
+interface MobileOption {
+  icon: JSX.Element;
+  name: string;
+}
+
+const mobileOptions: MobileOption[] = [
+  {
+    icon: <HomeIcon className={iconStyle} />,
+    name: 'Home',
+  },
+  {
+    icon: <RocketIcon className={iconStyle} />,
+    name: 'Launches',
+  },
+  {
+    icon: <SettingsIcon className={iconStyle} />,
+    name: 'Settings',
+  },
+];
+
+export default mobileOptions;
