14c03c95fcabdcfc2c19c500cf19ecb3e03d8933
Desparek <desparvek@gmail.com>
2023-03-31 11:49:40 +0200

    update nav bar file

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,15 +2,22 @@ import NavBarMobile from './NavBarMobile';
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-center p-4">
-      <div className="flex w-full flex-col justify-between">
-        <p className="flex items-center gap-x-2 font-medium">
-          <strong className="rounded-lg bg-primary-600 p-2 font-normal text-primary-100">
+    <nav className="flex items-center justify-center p-4">
+      <div className="flex w-full max-w-default justify-between p-2 font-medium">
+        <p className="flex items-center gap-x-2">
+          <strong className="rounded-lg bg-primary p-2 font-semibold">
             Space
           </strong>
           News
         </p>
-        <NavBarMobile />
+        <ul className="sr-only flex items-center gap-x-4 sm:not-sr-only">
+          <li>Home</li>
+          <li>Launches</li>
+          <li>Settings</li>
+        </ul>
+        <div className="sm:sr-only">
+          <NavBarMobile />
+        </div>
       </div>
     </nav>
   );
