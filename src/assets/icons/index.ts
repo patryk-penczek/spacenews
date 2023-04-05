3d648169592dce58036d769ee23f8d668bfc37d4
Desparek <desparvek@gmail.com>
2023-04-05 17:26:22 +0200

    add open icon to the project

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 export { ReactComponent as HomeIcon } from './home.svg';
+export { ReactComponent as OpenIcon } from './open.svg';
 export { ReactComponent as RocketIcon } from './rocket.svg';
 export { ReactComponent as SearchIcon } from './search.svg';
 export { ReactComponent as SettingsIcon } from './settings.svg';
