9058d480b8228383cb28bfe1e19218da5a08e7e5
Desparek <desparvek@gmail.com>
2023-04-07 00:12:52 +0200

    add react router to the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import '@/styles/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
