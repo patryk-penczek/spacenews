689cfc433815b098a35e81e0f4c0d443a781a26c
Desparek <desparvek@gmail.com>
2023-04-07 00:12:22 +0200

    add default layout to the home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import HomePageContent from '@/components/HomePageContent';
+import DefaultLayout from 'layouts/DefaultLayout';
+
+const HomePage = () => {
+  return (
+    <DefaultLayout>
+      <HomePageContent />
+    </DefaultLayout>
+  );
+};
+
+export default HomePage;
