4b6325eefea9882ae9092522a45fa9239226a6f9
patryk-penczek <desparvek@gmail.com>
2023-04-07 13:17:02 +0200

    remove line clamp from tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -44,8 +44,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('prettier-plugin-tailwindcss'),
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [require('prettier-plugin-tailwindcss')],
 };
