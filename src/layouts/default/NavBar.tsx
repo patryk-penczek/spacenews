18bc0cf6ec31aeaec762b0c4b7487d0baa2c86f0
Desparek <desparvek@gmail.com>
2023-04-08 00:04:56 +0200

    move navbarmobile to the navbar component

--- src/layouts/default/NavBar.tsx
+++ src/layouts/default/NavBar.tsx
@@ -1,7 +1,8 @@
 import mobileOptions from '@/data/mobileOptions';
-import NavBarMobile from './NavBarMobile';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavBar = () => {
+  const location = useLocation();
   return (
     <nav className="flex items-center justify-center bg-darkmode-400">
       <div className="flex w-full max-w-default justify-between p-4 font-medium text-grayscale-100">
@@ -11,14 +12,26 @@ const NavBar = () => {
           </strong>
           News
         </p>
-        <ul className="sr-only flex items-center gap-x-4 sm:not-sr-only">
-          {mobileOptions.map(({ name }) => {
-            return <li key={name}>{name}</li>;
+        <ul className="fixed bottom-0 left-0 grid w-full grid-cols-3 grid-rows-1 items-center bg-darkmode-400 text-darkmode-200 md:static md:flex md:justify-end md:gap-x-4 md:text-white">
+          {mobileOptions.map(({ icon, name, link }) => {
+            const activeItem = location.pathname === link ? 'text-primary' : '';
+            return (
+              <li
+                key={name}
+                className={`hover:text-primary hover:duration-300 ${activeItem}`}
+              >
+                <Link
+                  to={link}
+                  className="flex flex-col items-center justify-center py-3 md:p-0"
+                >
+                  {icon}
+                  {name}
+                </Link>
+              </li>
+            );
           })}
         </ul>
-        <div className="sm:sr-only">
-          <NavBarMobile />
-        </div>
+        <div className="sm:sr-only"></div>
       </div>
     </nav>
   );
