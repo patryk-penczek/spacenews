3ca99188c32e51b23cb5a938108690da57e0e298
Desparek <desparvek@gmail.com>
2023-04-09 00:03:15 +0200

    add reports page component

---
+++ src/pages/ReportsPage.tsx
@@ -0,0 +1,12 @@
+import ReportsPageContent from '@/components/ReportsPageContent';
+import DefaultLayout from 'layouts/DefaultLayout';
+
+const ReportsPage = () => {
+  return (
+    <DefaultLayout>
+      <ReportsPageContent />
+    </DefaultLayout>
+  );
+};
+
+export default ReportsPage;
