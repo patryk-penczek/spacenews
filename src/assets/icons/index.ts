253910c55ca7bbf3b77a25ec913e5c496bbc808a
Desparek <desparvek@gmail.com>
2023-04-08 23:54:44 +0200

    update svg icons and index.ts file

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,6 @@
-export { ReactComponent as AstronautIcon } from './astronaut.svg';
+export { ReactComponent as CrossIcon } from './cross.svg';
 export { ReactComponent as HomeIcon } from './home.svg';
 export { ReactComponent as OpenIcon } from './open.svg';
-export { ReactComponent as RocketIcon } from './rocket.svg';
+export { ReactComponent as ReportIcon } from './report.svg';
 export { ReactComponent as SearchIcon } from './search.svg';
 export { ReactComponent as SettingsIcon } from './settings.svg';
