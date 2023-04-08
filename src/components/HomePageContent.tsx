10dffea3e877c5f861e238ae8be0220db9e024ef
Desparek <desparvek@gmail.com>
2023-04-09 00:04:31 +0200

    add search bar to the home page component

--- src/components/HomePageContent.tsx
+++ src/components/HomePageContent.tsx
@@ -1,6 +1,7 @@
 import { OpenIcon } from '@/assets/icons';
 import { useEffect, useState } from 'react';
 import { getAllArticles } from '../../api/api';
+import SearchBar from './SearchBar';
 
 const HomePageContent = () => {
   const [articles, setArticles] = useState<ArticlesData>();
@@ -9,6 +10,7 @@ const HomePageContent = () => {
   }, []);
   return (
     <section className="flex w-full flex-col items-center bg-darkmode-400">
+      <SearchBar setArticles={setArticles} />
       {articles !== undefined &&
         articles.results.map((result, index) => {
           const datetime = result.published_at;
@@ -16,7 +18,7 @@ const HomePageContent = () => {
           return (
             <article
               key={index}
-              className="grid w-full max-w-default grid-cols-12 grid-rows-1 items-center justify-center gap-x-2 p-4 text-sm font-light text-white sm:gap-x-4 md:gap-x-8"
+              className="grid w-full max-w-default grid-cols-12 grid-rows-1 items-center justify-center gap-x-2 p-4 font-light text-white sm:gap-x-4 md:gap-x-8"
             >
               <a
                 href={result.url}
