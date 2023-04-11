3a593f12f8432ba78e656019400f00efe1965492
Desparek <desparvek@gmail.com>
2023-04-11 02:44:10 +0200

    add arrow svgs to the project

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,5 @@
+export { ReactComponent as ArrowLeftIcon } from './arrowleft.svg';
+export { ReactComponent as ArrowRightIcon } from './arrowright.svg';
 export { ReactComponent as CrossIcon } from './cross.svg';
 export { ReactComponent as HomeIcon } from './home.svg';
 export { ReactComponent as OpenIcon } from './open.svg';
