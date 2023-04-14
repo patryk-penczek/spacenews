4c1343e526e430a073ea8fedcd295a97959163c9
Desparek <desparvek@gmail.com>
2023-04-14 14:38:55 +0200

    add settings page component

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import SettingsPageContent from '@/components/SettingsPageContent';
+import DefaultLayout from 'layouts/DefaultLayout';
+
+const SettingsPage = () => {
+  return (
+    <DefaultLayout>
+      <SettingsPageContent />
+    </DefaultLayout>
+  );
+};
+
+export default SettingsPage;
