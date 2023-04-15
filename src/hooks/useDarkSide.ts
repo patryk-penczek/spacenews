07d17055e26776d310d9356e324d8f86dd0bafeb
Desparek <desparvek@gmail.com>
2023-04-15 12:29:37 +0200

    add useDarkSide hook

---
+++ src/hooks/useDarkSide.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+const useDarkSide = (): [
+  string,
+  React.Dispatch<React.SetStateAction<string>>,
+] => {
+  const [theme, setTheme] = useState<string>(localStorage.theme);
+  const colorTheme = theme === 'dark' ? 'light' : 'dark';
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+    localStorage.setItem('theme', theme);
+  }, [theme, colorTheme]);
+
+  return [colorTheme, setTheme];
+};
+
+export default useDarkSide;
