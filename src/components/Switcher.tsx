830c1f1d081ccce29d56398ee5a52ebf1326c9ed
Desparek <desparvek@gmail.com>
2023-04-15 12:29:48 +0200

    add switcher component

---
+++ src/components/Switcher.tsx
@@ -0,0 +1,25 @@
+import useDarkSide from '@/hooks/useDarkSide';
+import { useState } from 'react';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
+const Switcher = () => {
+  const [colorTheme, setTheme] = useDarkSide();
+  const [darkSide, setDarkSide] = useState(
+    colorTheme === 'light' ? true : false,
+  );
+  const toggleDarkMode = (checked) => {
+    setTheme(colorTheme);
+    setDarkSide(checked);
+  };
+  return (
+    <DarkModeSwitch
+      checked={darkSide}
+      onChange={toggleDarkMode}
+      size={56}
+      moonColor="black"
+      sunColor="white"
+    />
+  );
+};
+
+export default Switcher;
