5275334cbfb16e4f72abf9a81bdc4c9fbcf95101
Desparek <desparvek@gmail.com>
2023-04-16 11:08:26 +0200

    add new component info page and delete settings

---
+++ src/pages/InfoPage.tsx
@@ -0,0 +1,12 @@
+import InfoPageContent from '@/components/InfoPageContent';
+import DefaultLayout from 'layouts/DefaultLayout';
+
+const InfoPage = () => {
+  return (
+    <DefaultLayout>
+      <InfoPageContent />
+    </DefaultLayout>
+  );
+};
+
+export default InfoPage;
