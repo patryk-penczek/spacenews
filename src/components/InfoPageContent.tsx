ef8723544b9a0ea7ab76ac48d246ab022938fd3f
Desparek <desparvek@gmail.com>
2023-04-20 09:45:48 +0200

    add news pages to the info page content

--- src/components/InfoPageContent.tsx
+++ src/components/InfoPageContent.tsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from 'react';
+import { getAllInfo } from '../../api/api';
+import InfoSkeleton from './InfoSkeleton';
+
 const InfoPageContent = () => {
+  const [info, setInfo] = useState<any>();
+  useEffect(() => {
+    getAllInfo().then((data) => setInfo(data));
+  }, []);
+  const infoMap = info?.news_sites;
   return (
-    <section className="flex w-full flex-col items-center gap-y-4 bg-grayscale-100 dark:bg-darkmode-400">
-      <div className="text-black dark:text-white">
-        Created by{' '}
-        <a
-          href="https://patrykpenczek.pl"
-          target="_blank"
-          className="text-primary hover:opacity-80"
-          rel="noreferrer"
-        >
-          Patryk Penczek
-        </a>
+    <section className="flex min-h-[100vh] w-full flex-col items-center bg-grayscale-100 dark:bg-darkmode-400">
+      <div className="grid w-full max-w-default grid-cols-12 gap-y-4 p-4 md:gap-x-4 lg:gap-8">
+        {Array.isArray(infoMap) && infoMap.length > 0 ? (
+          infoMap.map((result, index) => {
+            return (
+              <a
+                key={index}
+                href={`https://www.google.com/search?q=${result}`}
+                target="_blank"
+                rel="noreferrer"
+                className="col-span-4 text-xs text-black hover:text-primary hover:duration-300 dark:text-white dark:hover:text-primary md:col-span-2 md:text-sm"
+              >
+                {result}
+              </a>
+            );
+          })
+        ) : (
+          <>
+            {Array.from({ length: 35 }).map((_, index) => {
+              return <InfoSkeleton key={index} />;
+            })}
+          </>
+        )}
       </div>
     </section>
   );
