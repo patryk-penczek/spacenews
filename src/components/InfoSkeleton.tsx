9f01077b1073208862a4ec646298eb8a7c87903a
Desparek <desparvek@gmail.com>
2023-04-20 09:45:21 +0200

    add component info skeleton for skeleton loading

---
+++ src/components/InfoSkeleton.tsx
@@ -0,0 +1,7 @@
+const InfoSkeleton = () => {
+  return (
+    <div className="col-span-4 my-2 h-3 w-20 rounded-md bg-grayscale-300 dark:bg-darkmode-200 sm:my-0 sm:h-4 sm:w-24 md:col-span-2 md:h-5 md:w-28"></div>
+  );
+};
+
+export default InfoSkeleton;
