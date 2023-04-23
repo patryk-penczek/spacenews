a68f2da59f140978b8ed1c8a4cc0c0a3718a6af0
Desparek <desparvek@gmail.com>
2023-04-23 22:46:24 +0200

    add info page hero component

---
+++ src/components/InfoPage/InfoPageHero.tsx
@@ -0,0 +1,29 @@
+const InfoPageHero = () => {
+  return (
+    <div className="flex w-full flex-col gap-y-2 text-justify text-sm md:text-base">
+      <h1 className="text-xl md:text-2xl">About Space News</h1>
+      <p>
+        Space is an incredibly fascinating area that has intrigued humanity for
+        centuries. Thanks to scientific and technological advancements in recent
+        years, space exploration has become possible in ways that were once
+        considered impossible. There are now advanced scientific instruments in
+        space that collect data and transmit information back to Earth, allowing
+        us to uncover the mysteries of our galaxy and expand our knowledge of
+        the Universe
+      </p>
+      <p>
+        If you're a space enthusiast, then Space News is the perfect website for
+        you. Our website provides access to the latest news and reports on space
+        exploration. You can follow the latest events and discoveries, learn
+        about the history of space exploration, and its impact on the
+        development of science and technology.
+      </p>
+      <p>
+        So, if you're interested in space and want to stay up-to-date with the
+        latest developments, Space News is the website for you!
+      </p>
+    </div>
+  );
+};
+
+export default InfoPageHero;
