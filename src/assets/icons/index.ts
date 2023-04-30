d93f7d2ed6290956fe778b42e782593721004228
patryk-penczek <desparvek@gmail.com>
2023-04-30 09:17:35 +0200

    add settings svg for search settings config

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -11,3 +11,4 @@ export { ReactComponent as OpenIcon } from './open.svg';
 export { ReactComponent as ReportIcon } from './report.svg';
 export { ReactComponent as SearchIcon } from './search.svg';
 export { ReactComponent as TwitterIcon } from './twitter.svg';
+export { ReactComponent as SettingsIcon } from './settings.svg';
