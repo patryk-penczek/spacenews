0e49745fc6c895d928e3f8997ff3d432c3b76e56
patryk-penczek <desparvek@gmail.com>
2023-05-03 12:51:01 +0200

    add search settings to the search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,17 +1,19 @@
-import { CrossIcon, SearchIcon } from '@/assets/icons';
-import { useRef } from 'react';
+import { CrossIcon, SearchIcon, SettingsIcon } from '@/assets/icons';
+import { useRef, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import { getAllArticles, getSearchedArticles } from '../../api/api';
+import SearchSettings from './SearchSettings';
 
 const SearchBar = ({ setArticles }: SetArticlesData) => {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const searchRef = useRef<HTMLInputElement | null>(null);
   const handleSubmit = (event: { preventDefault: () => void }) => {
     const inputValue = String(searchRef.current?.value);
     const title = inputValue.split(' ').join('%20');
     event.preventDefault();
-    navigate('/?page=1');
+    navigate('/');
     getSearchedArticles(title).then((data) => {
       if (data.results < 1) {
         toast.error('Sorry, no results found');
@@ -22,7 +24,7 @@ const SearchBar = ({ setArticles }: SetArticlesData) => {
   };
 
   return (
-    <div className="flex w-full max-w-default justify-center bg-grayscale-100 p-4 dark:bg-darkmode-400">
+    <div className="flex w-full max-w-default flex-col justify-center bg-grayscale-100 p-4 dark:bg-darkmode-400">
       <Toaster />
       <form className="flex w-full gap-x-4 rounded-md border-1 border-darkmode-400 bg-grayscale-100 p-3 drop-shadow-md dark:bg-darkmode-300 sm:p-4">
         <button onClick={handleSubmit}>
@@ -34,16 +36,23 @@ const SearchBar = ({ setArticles }: SetArticlesData) => {
           className="w-full bg-transparent text-black outline-none dark:text-white"
           ref={searchRef}
         />
-        <button
-          type="reset"
-          onClick={() => {
-            searchRef.current!.value = '';
-            getAllArticles().then((data) => setArticles(data));
-          }}
-        >
-          <CrossIcon className="h-6 w-6 text-grayscale-400 dark:text-darkmode-200" />
-        </button>
+        <div className="flex gap-x-2 text-grayscale-400 dark:text-darkmode-200 md:gap-x-4">
+          <button type="button" onClick={() => setIsOpen(!isOpen)}>
+            <SettingsIcon className="h-6 w-6 stroke-grayscale-400 dark:stroke-darkmode-200" />
+          </button>
+          <button
+            type="reset"
+            onClick={() => {
+              searchRef.current!.value = '';
+              navigate('/');
+              getAllArticles().then((data) => setArticles(data));
+            }}
+          >
+            <CrossIcon className="h-6 w-6" />
+          </button>
+        </div>
       </form>
+      <SearchSettings isOpen={isOpen} />
     </div>
   );
 };
