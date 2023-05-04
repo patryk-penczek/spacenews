6ca9b648cba55612623ff08189992bb1c5626984
Desparek <desparvek@gmail.com>
2023-05-04 09:36:00 +0200

    add href to the space news logo

--- src/layouts/default/NavBar.tsx
+++ src/layouts/default/NavBar.tsx
@@ -7,12 +7,12 @@ const NavBar = () => {
   return (
     <nav className="flex items-center justify-center bg-grayscale-100 dark:bg-darkmode-400">
       <div className="flex w-full max-w-default items-center justify-between gap-x-8 p-4 font-medium text-black dark:text-white">
-        <p className="flex items-center gap-x-2">
+        <Link to="/" className="flex items-center gap-x-2">
           <strong className="rounded-lg bg-primary p-2 font-semibold text-white">
             Space
           </strong>
           News
-        </p>
+        </Link>
         <ul className="fixed bottom-0 left-0 z-10 grid w-full grid-cols-3 grid-rows-1 items-center bg-grayscale-100 text-grayscale-400 dark:bg-darkmode-400 dark:text-darkmode-200 md:static md:flex md:justify-end md:gap-x-6 md:text-lg md:text-black dark:md:text-white">
           {mobileOptions.map(({ icon, name, link }) => {
             const activeItem = location.pathname === link ? 'text-primary' : '';
