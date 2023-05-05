cd46c8d13e097ac3091b938d6a1e09e5476e1719
Desparek <desparvek@gmail.com>
2023-05-05 18:22:55 +0200

    add error page component

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from 'layouts/DefaultLayout';
+import { ReactElement } from 'react';
+
+const NotFound = (): ReactElement => {
+  return (
+    <DefaultLayout>
+      <div>NotFound</div>
+    </DefaultLayout>
+  );
+};
+
+export default NotFound;
