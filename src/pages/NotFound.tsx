3c55ab7dfb87e8c5ede157521f03d74b36cdd5e7
patryk-penczek <desparvek@gmail.com>
2023-05-06 12:01:21 +0200

    add some styling to the error page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,18 @@
 import DefaultLayout from 'layouts/DefaultLayout';
 import { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = (): ReactElement => {
   return (
     <DefaultLayout>
-      <div>NotFound</div>
+      <div className="flex h-mobile w-full flex-col items-center justify-center gap-y-4 bg-grayscale-100 text-black dark:bg-darkmode-400 dark:text-white md:h-large md:gap-y-8">
+        <p className="text-3xl">Page not found...</p>
+        <Link to="/">
+          <button className="rounded-md bg-primary px-2 py-1 md:px-6 md:py-3">
+            Return to home
+          </button>
+        </Link>
+      </div>
     </DefaultLayout>
   );
 };
