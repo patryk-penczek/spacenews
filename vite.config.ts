ec51ac26ed4696532e3e2c1bcf760d7a2dfdc563
Desparek <desparvek@gmail.com>
2023-06-07 15:29:45 +0200

    add VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+    }),
+  ],
 });
