017e7a9563273abf8185df3ef1b1614086766d66
Desparek <desparvek@gmail.com>
2023-06-11 16:25:10 +0200

    update home page skeleton loading

--- src/components/ArticleSkeleton.tsx
+++ src/components/ArticleSkeleton.tsx
@@ -1,11 +1,9 @@
-import { ReactElement } from "react";
+import { ReactElement } from 'react';
 
 const ArticleSkeleton = (): ReactElement => {
   return (
-    <article className="grid w-full max-w-default animate-pulse grid-cols-12 items-center justify-center gap-x-2 p-4 sm:gap-x-4 md:gap-x-8">
-      <div className="col-span-5 h-full w-full">
-        <div className="aspect-[4/3] h-full max-h-80 w-full rounded-md bg-grayscale-300 dark:bg-darkmode-200" />
-      </div>
+    <article className="grid h-32 w-full max-w-default animate-pulse grid-cols-12 items-center justify-center gap-x-2 p-4 sm:h-40 sm:gap-x-4 md:h-56 md:gap-x-8 lg:h-72 xl:h-96">
+      <div className="col-span-5 h-full w-full rounded-md bg-grayscale-300 dark:bg-darkmode-200" />
       <div className="col-span-7 flex h-full flex-col justify-between gap-y-4">
         <header className="flex flex-col gap-y-2 md:gap-y-4">
           <div className="h-4 w-full rounded-md bg-grayscale-300 dark:bg-darkmode-200 md:h-8" />
