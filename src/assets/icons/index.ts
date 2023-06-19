e62fe7670ea3bd5e056b58c2074cd2ed9aeec6bb
Desparek <desparvek@gmail.com>
2023-06-19 17:20:18 +0200

    sort index file

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,7 @@
-export { ReactComponent as DoubleArrowLeftIcon } from './doublearrowleft.svg';
-export { ReactComponent as DoubleArrowRightIcon } from './doublearrowright.svg';
 export { ReactComponent as CrossIcon } from './cross.svg';
 export { ReactComponent as DarkModeIcon } from './darkmode.svg';
+export { ReactComponent as DoubleArrowLeftIcon } from './doublearrowleft.svg';
+export { ReactComponent as DoubleArrowRightIcon } from './doublearrowright.svg';
 export { ReactComponent as GithubIcon } from './github.svg';
 export { ReactComponent as HomeIcon } from './home.svg';
 export { ReactComponent as InfoIcon } from './info.svg';
@@ -10,5 +10,5 @@ export { ReactComponent as NasaIcon } from './nasa.svg';
 export { ReactComponent as OpenIcon } from './open.svg';
 export { ReactComponent as ReportIcon } from './report.svg';
 export { ReactComponent as SearchIcon } from './search.svg';
-export { ReactComponent as TwitterIcon } from './twitter.svg';
 export { ReactComponent as SettingsIcon } from './settings.svg';
+export { ReactComponent as TwitterIcon } from './twitter.svg';
