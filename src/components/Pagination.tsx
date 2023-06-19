8cd298abd526f54724f773bd2c1f686842737371
Desparek <desparvek@gmail.com>
2023-06-19 14:27:04 +0200

    remove unused classes and add sr-only

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -13,7 +13,7 @@ const PagePagination = ({
 }: PropTypes): ReactElement => {
   const arrowStyle = 'md:h-7 md:w-7 w-6 h-6';
   const buttonStyle =
-    'flex disabled:cursor-default disabled:cursor-not-allowed disabled:text-grayscale-300 dark:disabled:text-darkmode-200 items-center hover:text-primary hover:duration-300';
+    'flex disabled:sr-only items-center hover:text-primary hover:duration-300';
   const pageNumberWidth = 'w-8 text-center';
 
   const generateLinkButton = (page: number, content: ReactElement) => {
