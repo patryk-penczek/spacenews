7c413060cf95e53313535427a8e994425a581a64
patryk-penczek <desparvek@gmail.com>
2023-07-28 11:15:36 +0200

    refactor: refactor API calls using a common response handling function

--- api/api.ts
+++ api/api.ts
@@ -1,95 +1,44 @@
 const API_URL = 'https://api.spaceflightnewsapi.net/v4';
+const articles_limit = '?limit=100';
+
+const handleResponse = async (url: RequestInfo | URL) => {
+  try {
+    const response = await fetch(url);
+    if (response.ok) return response.json();
+    return {
+      success: false,
+      error: `Request failed with status code ${response.status}`,
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        success: false,
+        error: error.message,
+      };
+    }
+  }
+};
 
 export const getAllArticles = async () => {
-  try {
-    const response = await fetch(`${API_URL}/articles/?limit=100`);
-    if (response.ok) return response.json();
-    return {
-      success: false,
-      error: `Request failed with status code ${response.status}`,
-    };
-  } catch (error) {
-    if (error instanceof Error) {
-      return {
-        success: false,
-        error: error.message,
-      };
-    }
-  }
+  return handleResponse(`${API_URL}/articles/${articles_limit}`);
 };
 
-export const getSearchedArticles = async (title) => {
-  try {
-    const response = await fetch(
-      `${API_URL}/articles/?limit=100&title_contains=${title}`,
-    );
-    if (response.ok) return response.json();
-    return {
-      success: false,
-      error: `Request failed with status code ${response.status}`,
-    };
-  } catch (error) {
-    if (error instanceof Error) {
-      return {
-        success: false,
-        error: error.message,
-      };
-    }
-  }
+export const getSearchedArticles = async (title: string) => {
+  return handleResponse(
+    `${API_URL}/articles/${articles_limit}&title_contains=${title}`,
+  );
 };
 
-export const getSearchedNewsSites = async (news_site) => {
-  try {
-    const response = await fetch(
-      `${API_URL}/articles/?limit=100&news_site=${news_site}`,
-    );
-    if (response.ok) return response.json();
-    return {
-      success: false,
-      error: `Request failed with status code ${response.status}`,
-    };
-  } catch (error) {
-    if (error instanceof Error) {
-      return {
-        success: false,
-        error: error.message,
-      };
-    }
-  }
+export const getSearchedNewsSites = async (news_site: string) => {
+  return handleResponse(
+    `${API_URL}/articles/${articles_limit}&news_site=${news_site}`,
+  );
 };
 
 export const getAllReports = async () => {
-  try {
-    const response = await fetch(`${API_URL}/reports/?limit=6`);
-    if (response.ok) return response.json();
-    return {
-      success: false,
-      error: `Request failed with status code ${response.status}`,
-    };
-  } catch (error) {
-    if (error instanceof Error) {
-      return {
-        success: false,
-        error: error.message,
-      };
-    }
-  }
+  return handleResponse(`${API_URL}/reports/?limit=6`);
 };
 
 export const getAllInfo = async () => {
-  try {
-    const response = await fetch(`${API_URL}/info`);
-    if (response.ok) return response.json();
-    return {
-      success: false,
-      error: `Request failed with status code ${response.status}`,
-    };
-  } catch (error) {
-    if (error instanceof Error) {
-      return {
-        success: false,
-        error: error.message,
-      };
-    }
-  }
+  return handleResponse(`${API_URL}/info`);
 };
